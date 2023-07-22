fda44f103e7f4be81f8ec2c801c032e8c8827e71
megh2108 <20cp041@bvmenineering.ac.in>
2023-07-22 21:04:44 +0530

    one

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const app= express();
 app.use(cookieParser());
@@ -13,6 +14,10 @@ require('./db/conn');
 
 app.use(express.json());
 
+app.use(cors({
+    origin:"*"
+}))
+
 // for routing using express
 app.use(require('./router/auth'));
 // const User = require('./model/userSchema');
@@ -21,25 +26,35 @@ const PORT = process.env.PORT;
 // const DB = 'mongodb+srv://megh:Ban102002@cluster0.hmivwqh.mongodb.net/mernstack';
 
 
-const middelware = (req,res,next) =>{
-    console.log(`Hello Middelware.`)
-    next();
+// const middelware = (req,res,next) =>{
+//     console.log(`Hello Middelware.`)
+//     next();
+// }
+
+
+if(process.env.NODE_ENV === "production"){
+
+    app.use(express.static("client/build"));
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+    })
 }
-app.get('/', (req,res) =>{
-    res.send(`Hello World.`)
-});
+// app.get('/', (req,res) =>{
+//     res.send(`Hello World.`)
+// });
 
 // app.get('/about',middelware,(req,res)=>{
 //     res.send(`About hello world`)
 //     // console.log(`about..`)
 // })
-app.get('/contact',middelware,(req,res)=>{
-    res.send(`contact hello world`)
-})
-app.get('/home',(req,res)=>{
-    res.send(`home hello world`)
+// app.get('/contact',middelware,(req,res)=>{
+//     res.send(`contact hello world`)
+// })
+// app.get('/home',(req,res)=>{
+//     res.send(`home hello world`)
     
-})
+// })
 
 app.listen(PORT, () =>{
     console.log(`Server is running on ${PORT}.`)
